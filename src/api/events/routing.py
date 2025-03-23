9ec6114240416e9187a67432d5f5d0612ad34650
pedram <pedram.najafi73@gmail.com>
2025-03-23 23:24:12 +0330

    add list events item

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .schema import EventSchema
+from .schema import EventSchema, EventListSchema
 
 
 router = APIRouter()
@@ -7,8 +7,25 @@ router = APIRouter()
 @router.get("/")
 def read_events():
     return {
-        "items": [1,2,3]
+        "items":[1,2,3]
     }
+
+@router.get("/list-items/")
+def read_events() -> EventListSchema:
+    """ 
+        give data of all events.
+        Returns:
+            dict: the data of the events. 
+    """
+    return {
+        "items": [{
+                "id":1},
+            {
+                "id":2},
+            {
+                "id":3,}]
+    }
+
 
 @router.get("/{event_id}")
 def event(event_id: int) -> EventSchema :
