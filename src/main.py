2b5afbdea269f318e18ef937422aa2128d4b2369
pedram <pedram.najafi73@gmail.com>
2025-03-23 21:59:12 +0330

    add routing module in main app for endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from typing import Union
-
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI
+from api.events import router as events_router
 
 app = FastAPI()
+app.include_router(events_router,prefix="/api/events")
 
 
 @app.get("/")
@@ -13,3 +14,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/health")
+def read_api_health():
+    return {"status": "ok"}
